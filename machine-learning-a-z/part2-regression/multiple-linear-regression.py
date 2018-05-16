54c7d579f0a6b5280ff8f9c7f7f880bc7ee9e00a
Renato Porto <renatoafporto@gmail.com>
2018-05-16 19:10:21 +0000

    Create file to learn MLR with notes regarding the subject

---
+++ machine-learning-a-z/part2-regression/multiple-linear-regression.py
@@ -0,0 +1,18 @@
+#Multiple Linear Regression
+'''
+Multiple Linear Regression model
+
+y = b0 + b1 * x1 + b2 * x2 + b3 * x3 + ...
+
+y: dependent variable
+b0: constant
+bn: coefficients
+xn: independent variables
+
+Assumptions of a linear regression:
+1. Linearity
+2. Homoscedasticity
+3. Multivariate normality
+4. Independence of errors
+5. Lack of multicollinearity
+'''
