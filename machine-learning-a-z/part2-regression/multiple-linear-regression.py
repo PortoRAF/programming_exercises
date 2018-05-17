41c3cc4553a6cddf866bfc4180cc95f21f4e69d9
Renato Porto <renatoafporto@gmail.com>
2018-05-17 14:45:43 +0000

    Implement multiple linear regression

--- machine-learning-a-z/part2-regression/multiple-linear-regression.py
+++ machine-learning-a-z/part2-regression/multiple-linear-regression.py
@@ -25,10 +25,42 @@ Methods of building models:
 '''
 #import libraries
 import pandas as pd
+import numpy as nd
+import matplotlib.pyplot as plt
 
 #load dataset
 dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,4].values
 
-#'State' column is categorical and must be split into dummy variables
+#encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#avoid the Dummy Variable Trap
+#Remove the first column to avoid redundant dependencies
+#Python library for linear regression takes care for the Dummy Variable Trap, 
+#but that's not the case for other libraries
+X = X[:, 1:]
+
+#split the data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=0)
+
+'''
+There's no need to use feature scaling in multiple linear regression, Python
+library deals with it automatically
+'''
+
+#fit Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() #create a linear regression object
+regressor.fit(X_train, y_train) #fit the object to the training set
+
+#predict the Test Set results
+y_pred = regressor.predict(X_test)
