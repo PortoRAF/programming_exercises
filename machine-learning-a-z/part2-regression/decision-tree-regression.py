d4d4aaf05ebcbd0f2c65b39f6d84302fb3ad1f03
Renato Porto <renatoafporto@gmail.com>
2018-05-25 15:11:48 +0000

    Complete Decision Tree Regression model

--- machine-learning-a-z/part2-regression/decision-tree-regression.py
+++ machine-learning-a-z/part2-regression/decision-tree-regression.py
@@ -4,17 +4,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv("")
-X = dataset.iloc[:,:-1].values
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1:2].values
 y = dataset.iloc[:,2].values
-
+'''
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.2,
                                                     random_size=0)
-
+'''
 #fit Decision Tree Regression to dataset
-#Create your regressor here
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
 
 #predict a new result
 y_pred = regressor.predict(6.5)
@@ -22,7 +24,7 @@ y_pred = regressor.predict(6.5)
 #plot results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Regression Model')
+plt.title('Decision Tree Regression')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
@@ -32,7 +34,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Regression Model')
+plt.title('Decision Tree Regression')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
