8ab5cfb69559f6bf45f975cd579f2e4738fc414d
Renato Porto <renatoafporto@gmail.com>
2018-05-30 04:50:43 +0000

    Add spoj problems

--- c/math-operators/main.c
+++ c/math-operators/main.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-	float a = 21.0;
-	double b = 21.0;
+	int age1, age2, age3;
+	
+	age1 = 10;
+	age2 = ++age1; //increment age1 and assign incremented value to age2
+	age3 = age1++; //assign age1 value to age3 then increment age1
 
-	if (a==b) {
-		printf("They are equal\n");
-	} else {
-		printf("They are not equal\n");
-	}
+	printf("%d\n%d\n%d\n", age1, age2, age3);
+
+	age1 = age1++;	
+	printf("\n%d\n", age1);
+
+	age1 = ++age1;	
+	printf("\n%d\n", age1);
 
 	return 0;
 }
--- spoj/palin/main.c
+++ spoj/palin/main.c
@@ -1,35 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define MAX_NUM_DIGITS 1000000
 
+bool allNines (char *input, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++) {
+		int ia = input[i] - '0'; //Convert char to int
+		if (ia != 9) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+char *increment (int size)
+{
+	if (size == MAX_NUM_DIGITS) {
+		return "0";
+	}
+	
+	char *str = malloc(size+2);
+		
+	int i;
+	
+	for (i = 0; i < (size+2); i++) {
+		if (i==0 || i==size) {
+			str[i] = '1';
+		} else if (i==(size+1)) {
+			str[i] = '\0';
+		} else {
+			str[i] = '0';
+		}
+	}
+
+	return str;
+}
+
+char *palin (char input[])
+{
+	int i;
+	int size = (int)strlen(input);
+	bool nines = allNines(input, size);
+	
+	if (nines) {
+		return increment(size);
+	}
+
+	if (size == 1) {
+		input[0]++;
+		return input;
+	}
+	
+	if (size == 2) {
+		if (input[0] == input[1]) {
+			input[0]++;
+		}
+	}
+
+	int mid = size / 2;
+	int odd = size % 2;
+
+	if (input[mid-1] == input[mid+odd]) {
+		for (i = 0; i < mid; i++) {
+			if (input[mid-1-i] < input[mid+i+odd]) {
+				input[mid-1]++;
+				break;
+			}
+			if (i == mid-1) {
+				input[mid-1+odd]++;
+			}
+		}
+	} else if (input[mid-1] < input[mid+odd]) {
+		input[mid-1+odd]++;
+	}
+
+	for (i = 0; i < mid; i++) {
+		input[mid+i+odd] = input[mid-1-i];
+	}
+
+	return input;
+}
+
 int main ()
 {
-	char cases[3];
-	int t;
+	int t, i;
 
-	fgets(cases, 3, stdin);
-	t = strtol(cases, NULL, 10);
+	scanf("%d", &t);
 
 	if (t <= 0) {
 		return 0;
 	}
 
-	char inputs[t][MAX_NUM_DIGITS];
-	long long k[t];
-	int i;
-	
+	char *inputs[t];
 	for (i = 0; i < t; i++) {
-		fgets(inputs[i], MAX_NUM_DIGITS, stdin);
-		k[i] = strtoll(inputs[i], NULL, 10);
-		if (k[i] <= 0 || k[i] > powl(10,MAX_NUM_DIGITS)) {
+		inputs[i] = malloc(MAX_NUM_DIGITS * sizeof(char));
+	}
 
+	for (i = 0; i < t; i++) {
+		scanf("%s", inputs[i]);
+		if ((int)strlen(inputs[i]) > MAX_NUM_DIGITS) {
+			return 0;
 		}
 	}
 
 	for (i = 0; i < t; i++) {
-		printf("%lld\n", k[i]);
+		printf("%s\n", palin(inputs[i]));
 	}
 
 	return 0;
