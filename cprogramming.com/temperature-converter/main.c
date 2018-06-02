868c33d4471a1f0313a7277eb2f91065cbb8ab4b
Renato Porto <renatoafporto@gmail.com>
2018-06-02 20:12:01 +0000

    Add temperature converter exercise

---
+++ cprogramming.com/temperature-converter/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int check_limit (char *input, int value, int low, int high)
+{
+	if (value < low) {
+		printf("INVALID INPUT! %s temperature below limit!\n", input);
+		return 0;
+	} else if (value > high) {
+		printf("INVALID INPUT! %s temperature above limit!\n", input);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int main (void)
+{
+	int temp_C_low;
+	int temp_C_high;
+	int temp_C_step;
+
+	/*Take initial temperature in Celsius*/
+	printf("Please type in a lower limit (0 <= limit < 500): ");
+	scanf("%d", &temp_C_low);
+
+	if (!check_limit("Lower", temp_C_low, 0, 499)) return 0;
+	
+	/*Take final temperature in Celsius*/
+	printf("Please type in a higher limit (10 < limit <= 500): ");
+	scanf("%d", &temp_C_high);
+
+	if (!check_limit("Higher", temp_C_high, 11, 500)) return 0;
+	
+	/*Take step size*/
+	printf("Please type in a step (0 < step <= 50): ");
+	scanf("%d", &temp_C_step);
+
+	if (!check_limit("Step", temp_C_step, 1, 50)) return 0;
+
+	/*Check if step is smaller than temperature difference*/
+	if (temp_C_high - temp_C_low < temp_C_step) {
+		printf("INVALID INPUT! Step size is greater than temperature difference\n");
+		return 0;
+
+	}
+
+	int num_of_steps = (temp_C_high - temp_C_low) / temp_C_step; //Already includes initial value
+
+	int temp_C_steps[num_of_steps+2];
+	int i;
+	for (i = 0; i < (num_of_steps+1); i++) {
+		temp_C_steps[i] = temp_C_low + temp_C_step * i;
+	}
+
+	printf("\nCelsius\n-------\n");
+
+	for (i = 0; i < (num_of_steps+1); i++) {
+		printf("%.6d\n", temp_C_steps[i]);
+	}
+	
+	return 0;
+}
