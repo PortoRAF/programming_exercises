b1387203c237d8abe5ad7d61bd1f74b86be806be
Renato Porto <renatoafporto@gmail.com>
2018-06-03 03:27:35 +0000

    Improve Permutation explanation and comments to the code

--- cprogramming.com/basic/permutation/main.c
+++ cprogramming.com/basic/permutation/main.c
@@ -17,6 +17,23 @@ void swap (char *x, char *y)
 	*y = tmp;
 }
 
+/*****************************
+ * To execute the permutation, we need a loop within a loop for 
+ * each letter of the string. For 'ABCD', take 'A' then loop through
+ * 'AB' which give us 'ABCD' and 'ABDC'. Next loop is 'AC', where we
+ * get 'ACBD' and 'ACDB'. When 'AB', 'AC', and 'AD' are done, take
+ * 'B', then 'BA', 'BC', 'BD', and so on.
+ *
+ *------------------------------------------------------
+ * Inputs are: array to be permuted, recursive loop depth, size of the array
+ *
+ * The outer loop will put all array elements on the first position
+ * Next loop will put the remaining elements in the second position
+ * Following loop will take remaining elements to the third position
+ * And so as necessary.
+ *
+ */
+
 void permute (char *arr, int place, int end)
 {
 	if (place == (end-1))
