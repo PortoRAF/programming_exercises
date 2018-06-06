92a905b26173d10b711c675aba8f0aac6dcac76b
Renato Porto <renatoafporto@gmail.com>
2018-06-06 02:44:57 +0000

    String search can evaluate if a string is a subset of another

--- cprogramming.com/intermediate/string-search/main.c
+++ cprogramming.com/intermediate/string-search/main.c
@@ -13,6 +13,43 @@ int calc_str_len (char *str)
 	return str_len;
 }
 
+int compare_strings (char *str_orig, char *subset, int str_orig_len, int subset_len)
+{
+//	int is_subset = 0;
+	int start_comparing = 0;
+	int i;
+	int j = 0;
+
+	for (i = 0; i < str_orig_len; i++)
+	{
+		if (j < subset_len)
+		{
+			if (subset[j] == str_orig[i])
+			{
+				start_comparing = 1;
+			}
+
+			if (start_comparing)
+			{
+				j++;
+			}
+			else
+			{
+				j = 0;
+			}
+		
+			start_comparing = 0;
+		}
+		else
+		{
+//			is_subset = 1;
+			break;
+		}
+	}
+
+	return j == subset_len;
+}
+
 int main (int argc, char *argv[])
 {
 	if (argc != 3)
@@ -30,7 +67,16 @@ int main (int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	int is_subset = compare_strings(argv[1], argv[2], first_str_len, second_str_len);
 
+	if (is_subset)
+	{
+		printf("\"%s\" is a subset of \"%s\"\n", argv[2], argv[1]);
+	}
+	else
+	{
+		printf("String is not a subset.\n");
+	}
 
 	return 0;
 }
