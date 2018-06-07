b48534ce5e72667bef932c819304f8284d04092c
Renato Porto <renatoafporto@gmail.com>
2018-06-07 01:40:45 +0000

    Add Reverse Linked List exercise implementation

---
+++ cprogramming.com/intermediate/reverse-linked-list/linked.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked.h"
+
+struct list *list_create (void)
+{
+	struct list *list = malloc(sizeof(struct list));
+
+	list->head = NULL;
+	list->tail = NULL;
+	list->size = 0;
+
+	return list;
+}
+
+void list_destroy (struct list *list)
+{
+	struct node *current = list->head;
+	struct node *next;
+
+	while (current != NULL)
+	{
+		next = current->next;		
+		free(current);
+		current = next;
+	}
+
+	free(list);
+}
+
+void list_print (struct list *list)
+{
+	struct node *node = list->head;
+
+	printf("{ ");
+
+	while (node != NULL)
+	{
+		if (node != list->tail)
+		{
+			printf("%d, ", node->data);
+		}
+		else
+		{
+			printf("%d }\n", node->data);
+		}
+
+		node = node->next;
+	}
+}
+
+void print_next (struct node *node)
+{
+	if (node->next == NULL)
+	{
+		printf ("{ %d, ", node->data);
+	}
+	else
+	{
+		print_next(node->next);
+		printf("%d, ", node->data);
+	}
+}
+
+void list_print_reverse (struct list *list)
+{
+	print_next(list->head);
+	printf("}\n");
+}
+
+void list_reverse (struct list *list)
+{
+	struct node *prev = NULL;
+	struct node *current = list->head;
+	struct node *next = NULL;
+	
+	list->tail = current;		//New list's tail pointer will be old list's head
+
+	while (current != NULL)
+	{
+		next = current->next;	//Store value for further use
+		current->next = prev;	//Set a new head for current node
+		prev = current;			//Store head pointer for next node 
+		current = next;			//Iterate through list
+	}
+
+	list->head = prev;			//Assign new last node to new list's head
+}
+
+void node_insert (struct list *list, int data)
+{
+	struct node *node = malloc(sizeof(struct node));
+
+	node->data = data;
+	node->next = NULL;
+
+	if (list->size == 0)
+	{
+		list->head = node;
+		list->tail = node;
+	}
+	else
+	{
+		list->tail->next = node;
+		list->tail = node;
+	}
+
+	list->size++;
+}
---
+++ cprogramming.com/intermediate/reverse-linked-list/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked.h"
+
+int main (void)
+{
+	struct list *list = list_create();
+
+	node_insert(list, 1);
+	node_insert(list, 2);
+	node_insert(list, 3);
+
+	list_print(list);
+
+	list_reverse(list);
+
+	list_print(list);
+
+	list_destroy(list);
+
+	return 0;
+}
