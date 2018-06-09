4e0e0cd18164a51a2f06e9d6735c225dfacadb31
Renato Porto <renatoafporto@gmail.com>
2018-06-09 01:24:48 +0000

    Implement full working program

--- spoj/factorial-trailing-zeros/main.c
+++ spoj/factorial-trailing-zeros/main.c
@@ -14,16 +14,13 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_TESTS 100000
 #define MAX_INPUT 1000000000
 
-int main (void)
+int calc_zeroes(n)
 {
-	int n;
-
-	printf("Insert number: ");
-	scanf("%d", &n);
-
 	int result = 0;
 
 	while (n > 0)
@@ -31,8 +28,86 @@ int main (void)
 		n /= 5;
 		result += n;
 	}
+
+	return result;
+}
+
+int main (void)
+{
+	int test_case;
+	
+	int len_max = 128; // Arbitrary value to initialize array
+	int current_size = len_max;
+
+	char *input = malloc(len_max);
+	
+	char c;
+	int i = 0;
+	while ( (c = getchar()) != '\n')
+	{
+		input[i++] = c;
+	}
+
+	input[i] = '\0';
+	test_case = strtol(input, &input, 10);
+
+	if (test_case > MAX_TESTS || test_case < 1)
+	{
+		return 1;
+	}
+
+	if (input != NULL) // Check if memory was allocated
+	{
+		int c = NULL;
+		int i = 0;
+		int ret = 0;
+		while ( ret < test_case && c != EOF )
+		{
+			c = getchar();
+			input[i++] = (char)c;
+
+			if (i == current_size)
+			{
+				current_size = len_max + i;
+				input = realloc(input, current_size);
+			}
+
+			if (input[i-1] == '\n') ret++;
+/*			
+			if (i == 1)
+			{
+				test_case = strtol(input, &input, 10);
+				if (test_case > MAX_TESTS) break;
+			}
+*/
+		}
+		
+		input[i] = '\0'; // Make array into string
+
+//		printf("%s\n", input);
+
+//		char *str;
+//		test_case = strtol(input, &input, 10);i
+
+		for (i = 0; i < test_case; i++)
+		{
+			printf("%d\n", calc_zeroes(strtol(input, &input, 10)));
+		}
+	}
+	else
+	{
+		return(1);
+	}
+
+	int result = 0;
+/*
+	while (n > 0)
+	{
+		n /= 5;
+		result += n;
+	}
 	
 	printf("Z(n) = %d\n", result);
-
+*/
 	return 0;
 }
